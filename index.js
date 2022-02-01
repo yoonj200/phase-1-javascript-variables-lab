906daed6d596f5034d5983a2ad51cfe03f38a1c5
Joshua <yoonj200@gmail.com>
2022-02-01 11:51:06 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// companyName
+const companyName = 'Scuber';
+
+// mostProfitableNeighborhood
+let mostProfitableNeighborhood = "Chelsea";
+
+// companyCeo
+let companyCeo = "Susan Smith";
